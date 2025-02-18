16060fd90b54035ac5a76c4490ae6b5be650b86e
Robotics-PEC <111312267+Robotics-PEC@users.noreply.github.com>
2025-02-18 12:54:48 +0530

    Fixed another case error

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
       <BrowserRouter basename={(import.meta.env.MODE === "development") ? "/" : "/Studies"}>
       <Routes>
         <Route path="/" element={<Index />} />
-        <Route path="/upload" element={<Upload />} />
+        <Route path="/Upload" element={<Upload />} />
         {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
         <Route path="*" element={<NotFound />} />
       </Routes>
